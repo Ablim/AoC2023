882ddaaffd8dcec2eaf47b0d3e4a50eb7bd52b3e
Albin Bramstång <a.bramstang@gmail.com>
2023-11-17 20:27:06 +0100

    Prepare

---
+++ AoC2023/Day1/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace AoC2023.Day1
+{
+    public class Solution
+    {
+        public static int Day => 1;
+
+        public static string SolvePart1(string[] rows)
+        {
+            return string.Empty;
+        }
+
+        public static string SolvePart2(string[] rows)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ AoC2023/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Net;
+using AoC2023.Day1;
+
+var inputFile = $"Input{Solution.Day}.txt";
+
+if (!File.Exists(inputFile))
+{
+    var container = new CookieContainer();
+    var sessionCookie = await File.ReadAllTextAsync(".adventofcode.com");
+    container.Add(new Cookie("session", sessionCookie, string.Empty, ".adventofcode.com"));
+    var handler = new HttpClientHandler
+    {
+        CookieContainer = container
+    };
+
+    var client = new HttpClient(handler);
+    var response = await client.GetAsync($"https://adventofcode.com/2023/day/{Solution.Day}/input");
+    var bytes = await response.Content.ReadAsByteArrayAsync();
+    await File.WriteAllBytesAsync(inputFile, bytes);
+}
+
+var data = await File.ReadAllLinesAsync(inputFile);
+
+Console.WriteLine("Advent of Code 2023");
+Console.WriteLine($"Day {Solution.Day}");
+Console.WriteLine();
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+var part1 = Solution.SolvePart1(data);
+stopwatch.Stop();
+Console.WriteLine($"Part 1 ({stopwatch.ElapsedMilliseconds} ms)");
+Console.WriteLine(part1);
+Console.WriteLine();
+
+stopwatch.Restart();
+var part2 = Solution.SolvePart2(data);
+stopwatch.Stop();
+Console.WriteLine($"Part 2 ({stopwatch.ElapsedMilliseconds} ms)");
+Console.WriteLine(part2);
